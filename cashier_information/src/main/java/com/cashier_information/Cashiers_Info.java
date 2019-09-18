df7bddf2e1612a13af9d71de83c78eef989f9fd4
VIRAK33 <seamvirak.itc@gmail.com>
2019-09-18 22:57:15 +0700

    Initial commit

---
+++ cashier_information/src/main/java/com/cashier_information/Cashiers_Info.java
@@ -0,0 +1,23 @@
+package com.cashier_information;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.cashiers_info_fragment.R;
+
+public class Cashiers_Info extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment,container, false);
+        return view;
+    }
+}
+
+
